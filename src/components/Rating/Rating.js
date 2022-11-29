4f1fe2626afacdb9427b49ca5d30c5ef98725267
florian maugan <florian.maugan@gmail.com>
2022-11-29 17:13:36 +0100

    changement package.json et débug page error

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import '../../styles/Rating.css';
 
-// import { FaStar, FaRegStar } from 'react-icons/fa';
-
 const Rating = ({rate}) => {
 
 
-    // !!!!!!!!! UTILISER FONT AWESOME !!!!!!!!
     const note = []
     for (let i = 0; i < 5; i++) {
         if ( i + 1 <= rate) {
             note.push(<div key={i} className="active">
-                {/* <FaStar className="icon-reviews" /> */}
                 <i className="fas fa-star icon"></i>
-                {/* <i class="fas fa-star"></i> */}
                 </div>)
         } else {
             note.push(<div key={i} className="negative">
-                {/* <FaRegStar className="icon-reviews" /> */}
-                {/* <i className="fa-solid fa-star"></i> */}
-                {/* <i className="fa-regular fa-star"></i> */}
                 <i className="far fa-star icon"></i>
                 </div>)
         }
--- src/pages/Single/Single.js
+++ src/pages/Single/Single.js
@@ -1,8 +1,9 @@
-//import React, { useState, useEffect } from 'react';
 import Slider from '../../components/Slider/Slider';
 import Accordion from '../../components/Accordeon/Accordeon';
 import Rating from '../../components/Rating/Rating';
 import { useParams } from 'react-router-dom';
+
+import Error from '../404/404';
 
 import '../../styles/Single.css';
 
@@ -20,57 +21,61 @@ const Single = ({ data }) => {
         host,
         rating,
         tags,
-    } = dataID;
+    } = dataID || {};
 
-    console.log("holà", dataID)
+    // console.log("holà", dataID)
 
-    return (
-        <div className="container__single">
-            <div className="single__wrappe">
+    if (dataID !== undefined) {
+        return (
+            <div className="container__single">
+                <div className="single__wrappe">
 
-                <Slider pictures={pictures} />
-                <section className="single__content">
-                    <div className="single__content--header">
-                        <div className="title">
-                            <h1>{title}</h1>
-                            <address>{location}</address>
-                            <ul className="tags">
-                                {tags.map((tag, index) => {
-                                    return (
-                                        <li className="tag-item" key={index}>
-                                            {tag}
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        </div>
-                        <div className="user">
-                            <div className="user--name">
-                                <h3>{host.name}</h3>
-                                <img src={host.picture} alt={host.name} />
+                    <Slider pictures={pictures} />
+                    <section className="single__content">
+                        <div className="single__content--header">
+                            <div className="title">
+                                <h1>{title}</h1>
+                                <address>{location}</address>
+                                <ul className="tags">
+                                    {tags.map((tag, index) => {
+                                        return (
+                                            <li className="tag-item" key={index}>
+                                                {tag}
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
                             </div>
-                            <div className="reviews">
-                                <Rating rate={rating} />
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* ACCORDEON */}
-
-                    <div className='content'>
-                        <div className='accordions radius'>
-                            <div className='accordions_description width' >
-                                <Accordion title="Description" content={description} />
-                            </div>
-                            <div className='accordions_equipements width'>
-                                <Accordion title="Equipements" content={equipments} />
+                            <div className="user">
+                                <div className="user--name">
+                                    <h3>{host.name}</h3>
+                                    <img src={host.picture} alt={host.name} />
+                                </div>
+                                <div className="reviews">
+                                    <Rating rate={rating} />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </section>
+
+                        {/* ACCORDEON */}
+
+                        <div className='content'>
+                            <div className='accordions radius'>
+                                <div className='accordions_description width' >
+                                    <Accordion title="Description" content={description} />
+                                </div>
+                                <div className='accordions_equipements width'>
+                                    <Accordion title="Equipements" content={equipments} />
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return <Error />
+    }
 };
 
 export default Single;
